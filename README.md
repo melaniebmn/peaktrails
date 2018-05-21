776d363626ebd1adb85a21b7ada5738eac0f4d4b
Melanie Bockmann <melanie.bockmann@gmail.com>
2018-05-21 11:54:32 +0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Peaktrails
+
+Peaktrails is a website about hiking trails in and around Vancouver.
---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <main>
+          </main>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
